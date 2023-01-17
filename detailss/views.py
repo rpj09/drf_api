8e98754440474c7043ce86ae61923af07eb5bb90
“Ripunjay <singhripunjay09@gmail.com>
2023-01-18 00:09:28 +0530

    added jwt authentication

--- detailss/views.py
+++ detailss/views.py
@@ -1,15 +1,21 @@
 from django.http import JsonResponse
 from .models import Drinks
 from .serializers import DrinkSerializer
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import views
 from . import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
+
 
 @api_view(['GET','POST'])
+@permission_classes([IsAuthenticated])
 def drinklist(request,format=None):
+
+
 
     if request.method=='GET':
         drinks = Drinks.objects.all()
@@ -22,7 +28,9 @@ def drinklist(request,format=None):
             return Response(serializer.data,status=status.HTTP_201_CREATED)
 
 @api_view(['GET','PUT',"DELETE"])
+@permission_classes([IsAuthenticated])
 def details(request,id,format=None):
+
     try:
         drinks = Drinks.objects.get(pk=id)
     except Drinks.DoesNotExist:
@@ -39,4 +47,23 @@ def details(request,id,format=None):
     elif request.method == 'DELETE':
         drinks.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+class RegisterAPIView(views.APIView):
+    serializer_class = serializers.RegisterSerializer
+
+    def post(self,request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save()
+        
+            refresh = RefreshToken.for_user(user)
+
+            response_data = {
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+                'username': serializer.data['username'],
+                'email': serializer.data['email'],
+            }
+            return Response(response_data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
